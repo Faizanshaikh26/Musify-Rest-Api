8ecfef6e6cacaa7b460c1e03fa95ab2f30a8bcaa
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-06-17 11:10:41 +0530

    ll

--- models/Album.js
+++ models/Album.js
@@ -16,7 +16,6 @@ const albumSchema = new Schema({
     albumImage: { type: String, required: true },
     songs: [songSchema],
     bgcolor: { type: String, default: "#FFFFFF" },  
-    Bgcolor: { type: String, default: "#FFFFFF" },  
     created_at: { type: Date, default: Date.now }
 });
 
